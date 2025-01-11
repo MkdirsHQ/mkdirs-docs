62f6d94a8f6d414cac73d0c8b6d9df8f0afeb4cd
javayhu <javayhu@gmail.com>
2025-01-11 13:53:46 +0800

    chore: optimize docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,9 +26,8 @@ export default defineConfig({
 			locales: {
 				root: {
 					label: "English",
-					lang: "en", // lang is required for root locales
+					lang: "en",
 				},
-				// Simplified Chinese docs in `src/content/docs/zh-cn/`
 				"zh-cn": {
 					label: "简体中文",
 					lang: "zh-CN",
@@ -37,56 +36,184 @@ export default defineConfig({
 			sidebar: [
 				{
 					label: "Introduction",
+					translations: {
+						"zh-CN": "模板介绍"
+					},
 					slug: "index",
 				},
 				{
 					label: "Prerequisites",
+					translations: {
+						"zh-CN": "前提条件"
+					},
 					slug: "prerequisites",
 				},
 				{
 					label: "Installation",
+					translations: {
+						"zh-CN": "安装部署"
+					},
 					slug: "installation",
 				},
 				{
 					label: "Configuration",
+					translations: {
+						"zh-CN": "配置环境"
+					},
 					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: "Sanity", slug: "configuration/sanity" },
-						{ label: "Resend", slug: "configuration/resend" },
-						{ label: "Stripe", slug: "configuration/stripe" },
-						{ label: "Auth", slug: "configuration/auth" },
-						{ label: "Analytics", slug: "configuration/analytics" },
+						{
+							label: "Sanity",
+							translations: {
+								"zh-CN": "数据服务 - Sanity"
+							},
+							slug: "configuration/sanity"
+						},
+						{
+							label: "Resend",
+							translations: {
+								"zh-CN": "邮件服务 - Resend"
+							},
+							slug: "configuration/resend"
+						},
+						{
+							label: "Stripe",
+							translations: {
+								"zh-CN": "支付服务 - Stripe"
+							},
+							slug: "configuration/stripe"
+						},
+						{
+							label: "Auth",
+							translations: {
+								"zh-CN": "认证服务 - Auth"
+							},
+							slug: "configuration/auth"
+						},
+						{
+							label: "Analytics",
+							translations: {
+								"zh-CN": "数据分析 - Analytics"
+							},
+							slug: "configuration/analytics"
+						},
 					],
 				},
 				{
 					label: "Deployment",
+					translations: {
+						"zh-CN": "部署上线"
+					},
 					items: [
-						{ label: "Vercel", slug: "deployment/vercel" },
-						{ label: "Docker", slug: "deployment/docker" },
-						{ label: "Dokploy", slug: "deployment/dokploy" },
+						{
+							label: "Vercel",
+							translations: {
+								"zh-CN": "部署到 Vercel"
+							},
+							slug: "deployment/vercel"
+						},
+						{
+							label: "Docker",
+							translations: {
+								"zh-CN": "Docker 自部署"
+							},
+							slug: "deployment/docker"
+						},
+						{
+							label: "Dokploy",
+							translations: {
+								"zh-CN": "部署到 Dokploy"
+							},
+							slug: "deployment/dokploy"
+						},
 					],
 				},
 				{
 					label: "Customization",
+					translations: {
+						"zh-CN": "自定义"
+					},
 					items: [
-						{ label: "Information", slug: "customization/information" },
-						{ label: "Logo", slug: "customization/logo" },
-						{ label: "Font", slug: "customization/font" },
-						{ label: "Theme", slug: "customization/theme" },
-						{ label: "Card", slug: "customization/card" },
+						{
+							label: "Customize Information",
+							translations: {
+								"zh-CN": "自定义网站信息"
+							},
+							slug: "customization/information"
+						},
+						{
+							label: "Customize Logo",
+							translations: {
+								"zh-CN": "自定义 Logo"
+							},
+							slug: "customization/logo"
+						},
+						{
+							label: "Customize Font",
+							translations: {
+								"zh-CN": "自定义字体"
+							},
+							slug: "customization/font"
+						},
+						{
+							label: "Customize Theme",
+							translations: {
+								"zh-CN": "自定义主题"
+							},
+							slug: "customization/theme"
+						},
+						{
+							label: "Customize Card",
+							translations: {
+								"zh-CN": "自定义卡片样式"
+							},
+							slug: "customization/card"
+						},
+						{
+							label: "Customize Category",
+							translations: {
+								"zh-CN": "自定义分类"
+							},
+							slug: "customization/category"
+						},
+						{
+							label: "Customize Email",
+							translations: {
+								"zh-CN": "自定义邮件"
+							},
+							slug: "customization/email"
+						},
+						{
+							label: "Customize Page",
+							translations: {
+								"zh-CN": "自定义页面"
+							},
+							slug: "customization/custom-page"
+						},
 					],
 				},
 				{
 					label: "Guide",
+					translations: {
+						"zh-CN": "使用指南"
+					},
 					items: [
-						{ label: "Develop", slug: "guide/develop" },
-						{ label: "Email", slug: "guide/email" },
-						{ label: "Sanity", slug: "guide/sanity" },
-						{ label: "Category", slug: "guide/category" },
-						{ label: "Custom Page", slug: "guide/custom-page" },
+						{
+							label: "Development",
+							translations: {
+								"zh-CN": "开发相关"
+							},
+							slug: "guide/develop"
+						},
+						{
+							label: "Sanity",
+							translations: {
+								"zh-CN": "Sanity 相关"
+							},
+							slug: "guide/sanity"
+						},
 					],
 				},
-			],
+			]
 		}),
 	],
 });
